f0b33f9265331fbe675a18fc77c1ed5c6116780d
Faiza61 <shaukatfaiza61@gmail.com>
2024-04-01 11:00:12 +0500

    error removed

--- index.ts
+++ index.ts
@@ -9,18 +9,19 @@ import inquirer from "inquirer" ;
 
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 
-console.log("Welcome to number Guessing Game") ,
+console.log("Welcome to number Guessing Game") 
 
-const answers =await inquirer.prompt([
+ const answer =await inquirer.prompt([
+
 {
     name :"userGuessedNumber",
     type :"number",
     message :"Please guess a number between 1 -10 : ",
 }
 ]);
-console.log(answers); 
+console.log(answer); 
 //agar ya true ha to {iskoprint kare} warna else ko print kare
-if(answers.userGuessedNumber === randomNumber){
+if(answer.userGuessedNumber === randomNumber){
     console.log("Congratulations you guessed the Right Number.");
 }else {
     console.log("You guessed the Wrong Number");
